c7f62e673fdae3a851c9a9ec5ab39977ca6a2922
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-03 16:35:44 -0300

    feat: add User and Post models to the app

---
+++ internal/domain/user.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	Id        uuid.UUID      `gorm:"type:uuid;primaryKey;column:user_id" json:"id"`
+	Name      string         `gorm:"size:50;not null;column:user_name" json:"name"`
+	Email     string         `gorm:"size:255;not null;uniqueIndex;column:user_email" json:"email"`
+	IsActive  bool           `gorm:"not null;default:true;column:is_active" json:"is_active"`
+	CreatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP;column:created_at" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
+}
+
+func (u *User) TableName() string {
+	return "users"
+}
