8720ae82271aee4273837f3795a2bdeeda372cfe
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-04 11:34:33 -0300

    add: Validation if exists an unique constraint on (user_id, post_id) to likes table

--- db/migrations/000005_add_contraint_to_likes_table.up.sql
+++ db/migrations/000005_add_contraint_to_likes_table.up.sql
@@ -1,1 +1,13 @@
-ALTER TABLE likes ADD CONSTRAINT unique_user_post UNIQUE (user_id, post_id);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'unique_user_post'
+          AND conrelid = 'likes'::regclass
+    ) THEN
+        ALTER TABLE likes
+        ADD CONSTRAINT unique_user_post UNIQUE (user_id, post_id);
+    END IF;
+END;
+$$;
