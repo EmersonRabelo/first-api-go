dd11876a81038f0eb8aba7ad2c026a92a481615a
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-08 16:43:42 -0300

    feat: updated DTOs with new 'updated_at' and 'deleted_at' date fields

--- internal/dtos/post/update_dto.go
+++ internal/dtos/post/update_dto.go
@@ -1,6 +1,10 @@
 package post
 
+import "time"
+
 type PostUpdateDTO struct {
-	Body     *string `json:"body,omitempty"`
-	IsActive *bool   `json:"is_active,omitempty"`
+	Body      *string   `json:"body,omitempty"`
+	IsActive  *bool     `json:"is_active,omitempty"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
--- internal/dtos/user/update_dto.go
+++ internal/dtos/user/update_dto.go
@@ -1,7 +1,11 @@
 package user
 
+import "time"
+
 type UpdateDTO struct {
-	Name     string `json:"name"  binding:"min=3,max=50" example:"John Due"`
-	Email    string `json:"email" binding:"email" example:"johndue2025@email.com"`
-	IsActive bool   `json:"is_active" example:"true"`
+	Name      string    `json:"name"  binding:"min=3,max=50" example:"John Due"`
+	Email     string    `json:"email" binding:"email" example:"johndue2025@email.com"`
+	IsActive  bool      `json:"is_active" example:"true"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
