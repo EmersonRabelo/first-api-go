547bddc91c0eafb646684efdfb04935cf3afe4c6
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-08 11:30:13 -0300

    fix: argument name

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 type PostRepository interface {
-	Create(post *entity.Post)
+	Create(post *entity.Post) error
 	FindById(id uuid.UUID) (*entity.Post, error)
 	FindAll(page, pageSize int) ([]entity.Post, int64, error)
 	Update(post *entity.Post) error
@@ -22,22 +22,39 @@ func NewPostRepository(db *gorm.DB) PostRepository {
 	return &postRepository{db: db}
 }
 
-func (p *postRepository) Create(post *entity.Post) {
-	panic("unimplemented")
+func (p *postRepository) Create(post *entity.Post) error {
+	return p.db.Create(post).Error
 }
 
-func (p *postRepository) FindById(uuid uuid.UUID) (*entity.Post, error) {
-	panic("unimplemented")
+func (p *postRepository) FindById(id uuid.UUID) (*entity.Post, error) {
+	var post entity.Post
+
+	if err := p.db.First(&post, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &post, nil
 }
 
 func (p *postRepository) FindAll(page int, pageSize int) ([]entity.Post, int64, error) {
-	panic("unimplemented")
+	var posts []entity.Post
+	var amount int64
+
+	p.db.Model(&entity.Post{}).Count(&amount)
+
+	offset := (page - 1) * pageSize
+
+	if err := p.db.Offset(offset).Limit(pageSize).Find(&posts).Error; err != nil {
+		return nil, 0, err
+	}
+
+	return posts, amount, nil
 }
 
 func (p *postRepository) Update(post *entity.Post) error {
-	panic("unimplemented")
+	return p.db.Save(post).Error
 }
 
 func (p *postRepository) Delete(id uuid.UUID) error {
-	panic("unimplemented")
+	return p.db.Delete(p).Error
 }
