68b7f488b3ebbd1ab0b33b56cddf9a52fa007a2d
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-09 14:34:57 -0300

    add: Page attribute

--- internal/dtos/user/list_dto.go
+++ internal/dtos/user/list_dto.go
@@ -3,6 +3,7 @@ package user
 type UserResponseListDTO struct {
 	Data       []UserResponseDTO `json:"data"`
 	Total      int64             `json:"total"`
+	Page       int               `json:"page "`
 	PageSize   int               `json:"pageSize"`
 	TotalPages int               `json:"totalPages"`
 }
