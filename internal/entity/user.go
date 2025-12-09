7bf38547995a7ffbd7f3b6eb619f45ff084b8c32
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-09 14:36:51 -0300

    feat: Change type from deletedAt

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -4,17 +4,16 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type User struct {
-	Id        uuid.UUID      `gorm:"type:uuid;primaryKey;column:user_id" json:"id"`
-	Name      string         `gorm:"size:50;not null;column:user_name" json:"name"`
-	Email     string         `gorm:"size:255;not null;uniqueIndex;column:user_email" json:"email"`
-	IsActive  bool           `gorm:"not null;default:true;column:is_active" json:"is_active"`
-	CreatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP;column:created_at" json:"created_at"`
-	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
+	Id        uuid.UUID `gorm:"type:uuid;primaryKey;column:user_id" json:"id"`
+	Name      string    `gorm:"size:50;not null;column:user_name" json:"name"`
+	Email     string    `gorm:"size:255;not null;uniqueIndex;column:user_email" json:"email"`
+	IsActive  bool      `gorm:"not null;default:true;column:is_active" json:"is_active"`
+	CreatedAt time.Time `gorm:"not null;default:CURRENT_TIMESTAMP;column:created_at" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt time.Time `gorm:"column:deleted_at" json:"deleted_at"`
 }
 
 func (u *User) TableName() string {
