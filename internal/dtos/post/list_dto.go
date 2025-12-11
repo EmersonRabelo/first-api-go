18e4ac4995068fa1eae0cb4bf5659141ae3ea49a
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-11 19:25:56 -0300

    fix: dto references

--- internal/dtos/post/list_dto.go
+++ internal/dtos/post/list_dto.go
@@ -4,5 +4,6 @@ type PostResponseListDTO struct {
 	Data       []PostResponseDTO `json:"data"`
 	Total      int64             `json:"total"`
 	PageSize   int               `json:"pageSize"`
+	Page       int               `json:"page"`
 	TotalPages int               `json:"totalPages"`
 }
--- internal/dtos/post/update_dto.go
+++ internal/dtos/post/update_dto.go
@@ -3,8 +3,8 @@ package post
 import "time"
 
 type PostUpdateDTO struct {
-	Body      *string   `json:"body,omitempty"`
-	IsActive  *bool     `json:"is_active,omitempty"`
+	Body      string    `json:"body,omitempty"`
+	IsActive  bool      `json:"is_active,omitempty"`
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt time.Time `json:"deleted_at"`
 }
