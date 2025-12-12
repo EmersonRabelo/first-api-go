4b5b0db8999d2305a2dc945aebc5ad10bbe48ad2
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-12 12:31:30 -0300

    fix: changing struct params to pointers

--- internal/dtos/post/response_dto.go
+++ internal/dtos/post/response_dto.go
@@ -7,11 +7,11 @@ import (
 )
 
 type PostResponseDTO struct {
-	Id        uuid.UUID `json:"id"`
-	UserId    uuid.UUID `json:"user_id"`
-	Body      string    `json:"body"`
-	IsActive  bool      `json:"is_active"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	Id        uuid.UUID  `json:"id"`
+	UserId    uuid.UUID  `json:"user_id"`
+	Body      string     `json:"body"`
+	IsActive  bool       `json:"is_active"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
