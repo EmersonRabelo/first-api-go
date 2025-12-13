80a1c42e3cfe9f08f4c7c0bc42b98c9bc6bf197b
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-12 21:38:49 -0300

    fix: changing method arguments to pointers

--- internal/dtos/like/response_dto.go
+++ internal/dtos/like/response_dto.go
@@ -7,11 +7,11 @@ import (
 )
 
 type LikeResponseDTO struct {
-	Id        uuid.UUID `json:"id"`
-	UserId    uuid.UUID `json:"user_id"`
-	PostId    uuid.UUID `json:"post_id"`
-	Quantity  uint64    `json:"quantity"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	Id        uuid.UUID  `json:"id"`
+	UserId    uuid.UUID  `json:"user_id"`
+	PostId    uuid.UUID  `json:"post_id"`
+	Quantity  uint64     `json:"quantity"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
