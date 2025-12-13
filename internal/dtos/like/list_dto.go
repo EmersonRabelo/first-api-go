1c0c4ce13ca5e51af38684ca8926773492bb57b3
emerabelo <emerson.rabelo@mercadolivre.com>
2025-12-12 21:37:05 -0300

    add: Page param

--- internal/dtos/like/list_dto.go
+++ internal/dtos/like/list_dto.go
@@ -3,6 +3,7 @@ package like
 type LikeResponseListDTO struct {
 	Data       []LikeResponseDTO `json:"data"`
 	Total      int64             `json:"total"`
+	Page       int               `json:"page"`
 	PageSize   int               `json:"pageSize"`
 	TotalPages int               `json:"totalPages"`
 }
