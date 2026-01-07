8bd1cb147806574d4f564a0e5b1f756dd4616574
EmersonRabelo <rabelo.emerson1998@gmail.com>
2026-01-07 11:00:27 -0300

    feat: consume and persist the report analysis message

--- internal/repository/report_repository.go
+++ internal/repository/report_repository.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"github.com/EmersonRabelo/first-api-go/internal/entity"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type ReportRepository interface {
 	Create(context context.Context, report *entity.Report) error
 	UpdateReportStatus(context context.Context, report *entity.Report) error
+	FindById(id uuid.UUID) (*entity.Report, error)
+	Update(report *entity.Report) error
 }
 
 type reportRepository struct {
@@ -26,4 +29,18 @@ func (r *reportRepository) Create(context context.Context, report *entity.Report
 
 func (r *reportRepository) UpdateReportStatus(context context.Context, report *entity.Report) error {
 	return r.db.WithContext(context).Save(report).Error
+}
+
+func (r *reportRepository) FindById(id uuid.UUID) (*entity.Report, error) {
+	var report entity.Report
+
+	if err := r.db.First(&report, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &report, nil
+}
+
+func (r *reportRepository) Update(report *entity.Report) error {
+	return r.db.Save(report).Error
 }
--- internal/service/consumer/report_analysis_handler.go
+++ internal/service/consumer/report_analysis_handler.go
@@ -3,10 +3,12 @@ package consumer
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	contracts "github.com/EmersonRabelo/first-api-go/internal/dtos/report/message"
 	"github.com/EmersonRabelo/first-api-go/internal/entity"
 	"github.com/EmersonRabelo/first-api-go/internal/repository"
+	"github.com/google/uuid"
 )
 
 var ErrInvalidMessage = errors.New("invalid message")
@@ -26,6 +28,31 @@ func NewConsumerReportService(repository repository.ReportRepository) *ConsumerR
 }
 
 func (crs *ConsumerReportService) Create(msg contracts.ReportAnalysisResultMessage) error {
-	fmt.Println("Response: ", msg)
+
+	if msg.ReportId == uuid.Nil {
+		return fmt.Errorf("%w: Id is null", ErrInvalidMessage)
+	}
+
+	report, err := crs.repository.FindById(msg.ReportId)
+
+	if err != nil {
+		return errors.New("Report not find")
+	}
+
+	report.PerspectiveToxicity = msg.Toxicity
+	report.PerspectiveSevereToxicity = msg.SevereToxicity
+	report.PerspectiveIdentityAttack = msg.IdentityAttack
+	report.PerspectiveInsult = msg.Insult
+	report.PerspectiveProfanity = msg.Profanity
+	report.PerspectiveThreat = msg.Threat
+	report.PerspectiveLanguage = msg.Language
+	report.PerspectiveResponseAt = msg.AnalyzedAt
+	report.UpdatedAt = time.Now()
+	report.Status = entity.StatusDone
+
+	if err := crs.repository.Update(report); err != nil {
+		return errors.New("Error persist content analysis")
+	}
+
 	return nil
 }
